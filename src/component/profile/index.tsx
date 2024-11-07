63c05d81d128d9343a445f782dcd7ec0add58192
tnnquangvx <quang.tran@vongxanh.com>
2024-11-07 16:53:18 +0700

    .,

--- src/component/profile/index.tsx
+++ src/component/profile/index.tsx
@@ -1,4 +1,4 @@
-import ProfileConsoles from "./consoles";
+import ProfileConsoles from "./profile-consoles";
 import PersonalDetailComponent from "./personal-detail";
 
 export default function ProfileComponent() {
@@ -7,6 +7,17 @@ export default function ProfileComponent() {
       <PersonalDetailComponent />
       <h3 style={{ marginTop: "40px" }} />
       <ProfileConsoles />
+      <p style={{ marginTop: "40px" }}>
+        I divide the components as follows: - header (src/layout/header.tsx) is
+        the common header for the entire app. The profile page is divided into
+        smaller components: Including the index.tsx file to summarize the
+        components that I wrote for the profile. And: - Component
+        personal-detail (src/component/profile/personal-detail) - Component
+        profile-consoles (src/component/profile/consoles) Dividing like this
+        will be more effective because I know where each part is, easy to
+        maintain and extend without having to worry about other changes to the
+        entire profile page.
+      </p>
     </div>
   );
 }
